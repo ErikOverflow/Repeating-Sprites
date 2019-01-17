9bfc27ed72b3b0b849964c986aeeee335495a24f
ErikOverflow <46797334+ErikOverflow@users.noreply.github.com>
2019-01-17 15:12:59 -0600

    Add files for youtube tutorial

---
+++ RepeatingSprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingSprite : MonoBehaviour
+{
+    private RepeatingSpriteController RepeatingSpriteController;
+    private float width;
+
+    private void Awake()
+    {
+        RepeatingSpriteController = GetComponentInParent<RepeatingSpriteController>();
+    }
+
+    private void Start()
+    {
+        RepeatingSpriteController.SpriteExitedView(this);
+        width = GetComponent<SpriteRenderer>().size.x;
+    }
+
+    public float GetWidth()
+    {
+        return width;
+    }
+
+    private void OnBecameInvisible()
+    {
+        RepeatingSpriteController.SpriteExitedView(this);
+    }
+
+    private void OnBecameVisible()
+    {
+        RepeatingSpriteController.SpriteEnteredView(this);
+    }
+}
---
+++ RepeatingSpriteController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RepeatingSpriteController : MonoBehaviour
+{
+    private List<RepeatingSprite> spritesInView;
+    private List<RepeatingSprite> spritesOutOfView;
+    private float totalWidthOfSprites;
+
+    private void Awake()
+    {
+        spritesInView = new List<RepeatingSprite>();
+        spritesOutOfView = new List<RepeatingSprite>();
+    }
+
+    private void Start()
+    {
+        totalWidthOfSprites = 0;
+        foreach (RepeatingSprite bc in GetComponentsInChildren<RepeatingSprite>())
+        {
+            totalWidthOfSprites += bc.GetWidth();
+        }
+    }
+
+    private void MoveSprites(float lastSpritePosition)
+    {
+        if(spritesInView.Count > 0)
+        {
+            if (spritesInView.FirstOrDefault().transform.position.x > lastSpritePosition)
+            {
+                spritesOutOfView.OrderBy(b => b.transform.position.x).FirstOrDefault().transform.position += new Vector3(totalWidthOfSprites, 0, 0);
+            }
+            else
+            {
+                spritesOutOfView.OrderBy(b => b.transform.position.x).LastOrDefault().transform.position -= new Vector3(totalWidthOfSprites, 0, 0);
+            }
+        }
+    }
+
+    public void SpriteEnteredView(RepeatingSprite bc)
+    {
+        spritesInView.Add(bc);
+        spritesOutOfView.Remove(bc);
+    }
+
+    public void SpriteExitedView(RepeatingSprite bc)
+    {
+        spritesOutOfView.Add(bc);
+        spritesInView.Remove(bc);
+        MoveSprites(bc.transform.position.x);
+    }
+}
